cc389a849e07f66c4f03f4728d89aedd9ebe3870
Joey Clover <joey.clover@gmail.com>
2015-11-11 17:10:30 +0000

    Initial Project Setup

---
+++ Snap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snap
+//
+//  Created by Joey Clover on 11/11/2015.
+//  Copyright © 2015 Just Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Snap
+//
+//  Created by Joey Clover on 11/11/2015.
+//  Copyright © 2015 Just Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
