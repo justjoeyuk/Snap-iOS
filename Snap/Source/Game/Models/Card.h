2260235fee33034155fd6b2b969281cbd25f23f0
Joey Clover <joey.clover@gmail.com>
2015-11-11 18:42:26 +0000

    Changed Bridging Headers

---
+++ Snap/Source/Game/Models/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Snap
+//
+//  Created by Joey Clover on 11/11/2015.
+//  Copyright © 2015 Just Joey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Snap/Source/Game/Models/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Snap
+//
+//  Created by Joey Clover on 11/11/2015.
+//  Copyright © 2015 Just Joey. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
