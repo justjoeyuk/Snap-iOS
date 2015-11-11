07f8d180c6da9bdfea3731e78025c4e0043bc797
Joey Clover <joey.clover@gmail.com>
2015-11-11 20:10:45 +0000

    Added card view

---
+++ Snap/Source/Game/Views/Card/CardView.swift
@@ -0,0 +1,17 @@
+//
+//  CardView.swift
+//  Snap
+//
+//  Created by Joey Clover on 11/11/2015.
+//  Copyright © 2015 Just Joey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CardView: UIView {
+    
+    
+    
+}
